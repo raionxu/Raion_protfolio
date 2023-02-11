b1b47a3cf0373c66673b02b3ffb84ef738af713f
Raion Xu <122888016+raionxu@users.noreply.github.com>
2023-02-11 19:34:52 +0000

    1

---
+++ wcc1_come here/particle.js
@@ -0,0 +1,44 @@
+var s 
+class Particle{
+    constructor(){
+        this.pos = createVector(width/2, height/2)
+        this.vel = createVector(0,0)
+        this.acc = p5.Vector.random2D().normalize()
+        this.r = map(this.pos.x, 0, width, 255, 0)
+        this.g = map(this.pos.y, 0, height, 0, 255)
+        this.b = map(dist(width/2, height/2, this.pos.x, this.pos.y), 0, width/2, 0, 255)
+        this.alpha = 1
+        this.size = random(0,8)
+    }
+
+    update(){
+        var m = map(sin(frameCount), -1, 1, 0.4, 0.6)
+        this.acc.mult(m)
+
+        this.vel.add(this.acc)
+        this.pos.add(this.vel)
+
+        this.r = map(this.pos.x, 0, width, 255, 0)
+        this.g = map(this.pos.y, 0, height, 0, 255)
+        this.b = map(dist(width/2, height/2, this.pos.x, this.pos.y), 0, width/2, 0, 255)
+        
+        if(dist(width/2, height/2, this.pos.x, this.pos.y)>50)
+        {
+            this.alpha+=2
+        }    
+
+    }
+
+    show(){
+        noStroke()
+        fill(this.r, this.g, this.b, this.alpha)
+        if(mouseX<600 && mouseY<600 && mouseX>200 && mouseY>50)
+        {
+        ellipse(this.pos.x, this.pos.y, this.size)
+        }
+        else{
+            ellipse(this.pos.x, this.pos.y, this.size)
+            this.alpha -= 12
+        }
+    }
+}
---
+++ wcc1_come here/sketch.js
@@ -0,0 +1,91 @@
+
+var particle =[]
+var move = 0
+var changecolor = 200
+function preload() {
+	fontBold = loadFont('ADOBEARABIC-BOLD.OTF');
+  }
+
+function setup() {
+  createCanvas(800,800);
+	angleMode(DEGREES)
+	colorMode(HSB, 360, 100, 100, 100);
+	noiseDetail(2,1)
+	textAlign(CENTER, CENTER);
+	textFont(fontBold);
+}
+
+
+
+function draw() {
+
+    background(255)
+	
+
+	for(var i = 0; i<5; i++){
+		p = new Particle()
+		particle.push(p)
+	}
+ 
+
+	for(var i = 0; i<particle.length; i++){
+		if(particle[i].alpha>0){
+		particle[i].update()
+		particle[i].show()
+		}
+		else{
+			particle.splice(i,1)
+		}
+	}
+
+	translate (width/2,height/2)
+	fill(255,200)
+	circle(0,0,220)
+	
+	
+	if(mouseX>600 || mouseY>600 || mouseX<200 || mouseY<200)
+        {
+			changecolor -=60
+		}
+		fill (changecolor,70,200)
+		textSize(30);
+		text('COME HERE', 0, 0);
+
+	var space = 1
+	for(var i = 0; i<360; i+=space){
+		
+		var xoff = map(cos(i), -2, 1, 0, 3)
+		var yoff = map(sin(i), -2, 1, 0, 3)
+
+		var n = noise(xoff+move,yoff+move)
+		var h = map(n,0,1,-150,150)
+				var r1 = map(sin(i), -1, 1, 100, 200)
+		var g1 = map(h, -360,width,0,200)
+		var b1 = map(n,0,1,height,255)
+
+		if(mouseX>600 || mouseY>600 || mouseX<200 || mouseY<200)
+        {
+			changecolor = 100
+			var r1 = map(sin(i)*3, -1, 1, 100, 200)
+			var g1 = map(h, 0,width/2,0,200)
+			var b1 = map(n,0,1,height,255)
+		}
+		
+
+		fill (r1,g1,b1)
+		rotate (space)
+		rect (110,0,h/5,5)
+
+		
+		rotate (space)
+		rect (110,0,h,1)
+
+
+
+	}
+	move += 0.005
+
+}
+
+
+
