c801c39e05e10e08382991b59cd8e04d99d143e7
Raion Xu <122888016+raionxu@users.noreply.github.com>
2023-03-13 12:51:16 +0000

    network exp

---
+++ wcc2_networked experiences/public/javascript/sketch.js
@@ -0,0 +1,80 @@
+
+
+// Create connection to Node.JS Server
+const socket = io();
+
+let canvas;
+let roll = 0;
+let pitch = 0;
+let yaw = 0;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+ 
+  createEasyCam();
+ 
+}
+
+function draw() {
+  background(200);
+
+  noStroke();
+  lights();
+  ambientMaterial(100, 0, 100);
+
+  rotateZ(pitch);
+  rotateX(roll);
+  rotateY(yaw);
+  box(100);
+
+}
+
+//process the incoming OSC message and use them for our sketch
+function unpackOSC(message){
+
+  /*-------------
+
+  This sketch is set up to work with the gryosc app on the apple store.
+  Use either the gyro OR the rrate to see the two different behaviors
+  TASK: 
+  Change the gyro address to whatever OSC app you are using to send data via OSC
+  ---------------*/
+
+  //maps phone rotation directly 
+  // if(message.address == "/gyrosc/gyro"){
+  //   roll = message.args[0]; 
+  //   pitch = message.args[1];
+  //   yaw = message.args[2];
+  // }
+
+  //uses the rotation rate to keep rotating in a certain direction
+  if(message.address == "/gyrosc/rrate"){
+    roll += map(message.args[0],-3,3,-0.1,0.1);
+    pitch += map(message.args[1],-3,3,-0.1,0.1);
+    yaw += map(message.args[2],-3,3,-0.1,0.1);
+  }
+}
+
+//Events we are listening for
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// Connect to Node.JS Server
+socket.on("connect", () => {
+  console.log(socket.id);
+});
+
+// Callback function on the event we disconnect
+socket.on("disconnect", () => {
+  console.log(socket.id);
+});
+
+// Callback function to recieve message from Node.JS
+socket.on("message", (_message) => {
+
+  console.log(_message);
+
+  unpackOSC(_message);
+
+});
